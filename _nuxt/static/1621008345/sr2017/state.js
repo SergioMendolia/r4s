8ee61aa6f3be83d221defbea87081a085f4373f0
SergioMendolia <SergioMendolia@users.noreply.github.com>
2021-05-14 16:13:39 +0000

    deploy: b48cd60e55c88267facb6fbe5fe35e187635d42f

--- _nuxt/static/1621008345/sr2017/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621008345",layout:"default",error:a,serverRendered:false,routePath:"\u002Fsr2017",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"30359033"}}}}(null));
--- _nuxt/static/1621008345/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621008345",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"30359033"}}}}(null,"\u002F"));
