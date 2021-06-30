4525571d5a7e31357435c7807a40421026ca9eef
SergioMendolia <SergioMendolia@users.noreply.github.com>
2021-06-30 09:42:11 +0000

    deploy: d8ca656cba8b479dc028185bd65376c12aea41d5

---
+++ _nuxt/static/1625046129/sr2017/3-spread-the-net/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625046129",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2017\u002F3-spread-the-net",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"20fb63a7"}}}}(null));
