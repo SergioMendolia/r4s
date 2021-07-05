1e3dafa4d6cd0f4a485f21be080495f5a35bb222
SergioMendolia <SergioMendolia@users.noreply.github.com>
2021-07-05 05:07:50 +0000

    deploy: 35cddc035e967b54b3c4e66695801818d835ef65

--- _nuxt/static/1625461650/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625461650",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"22d2f500"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1625461668/sr2018/5-mirage/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625461668",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2018\u002F5-mirage",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c05ccd3d"}}}}(null));
