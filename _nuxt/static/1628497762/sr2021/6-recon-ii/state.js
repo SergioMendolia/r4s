68d960b2729ace409b7e8bff0a4328903b8748a6
SergioMendolia <SergioMendolia@users.noreply.github.com>
2021-08-09 08:29:24 +0000

    deploy: b671dddc3727fd3973923c80bad2b7500be4ee22

---
+++ _nuxt/static/1628497762/sr2021/6-recon-ii/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628497762",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2021\u002F6-recon-ii",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b809dce"}}}}(null));
---
+++ _nuxt/static/1628497762/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628497762",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b809dce"}}}}(null,"\u002F"));
