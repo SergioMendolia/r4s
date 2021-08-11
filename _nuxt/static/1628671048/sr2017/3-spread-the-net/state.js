4c424933bd9ee0588d1579220a99719f3732841c
SergioMendolia <SergioMendolia@users.noreply.github.com>
2021-08-11 08:37:31 +0000

    deploy: 6483bc022952f0678f894d75a27ba6e5ef0240df

---
+++ _nuxt/static/1628671048/sr2017/3-spread-the-net/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628671048",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2017\u002F3-spread-the-net",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5f1bf90b"}}}}(null));
---
+++ _nuxt/static/1628671048/sr2021/4-invasion/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628671048",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2021\u002F4-invasion",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5f1bf90b"}}}}(null));
