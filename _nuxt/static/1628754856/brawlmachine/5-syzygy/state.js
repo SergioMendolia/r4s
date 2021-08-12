31ae7751a25b18a4090752e6e7e903f7682de1ac
SergioMendolia <SergioMendolia@users.noreply.github.com>
2021-08-12 07:54:18 +0000

    deploy: 72ec85dad79947fcd16f243ec87211e70482f112

---
+++ _nuxt/static/1628754856/brawlmachine/5-syzygy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628754856",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbrawlmachine\u002F5-syzygy",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9e8d58f6"}}}}(null));
