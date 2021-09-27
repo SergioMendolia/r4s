4269694c04e62fbbf4f5f88f4e9ee63e7fff7a38
SergioMendolia <SergioMendolia@users.noreply.github.com>
2021-09-27 06:22:42 +0000

    deploy: bd9da6a4e04954c26fd76d8304e85d20ef03591a

--- _nuxt/static/1632723732/brawlmachine/2-binary/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632723732",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbrawlmachine\u002F2-binary",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d1b87ff2"}}}}(null));
--- _nuxt/static/1632723732/sr2019/1-king-of-the-hill/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632723732",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2019\u002F1-king-of-the-hill",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d1b87ff2"}}}}(null));
