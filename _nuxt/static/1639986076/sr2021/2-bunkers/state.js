4914e3f8230876b99491887fd7ec8ea2524da199
SergioMendolia <SergioMendolia@users.noreply.github.com>
2021-12-24 14:13:40 +0000

    deploy: 4780247ab68a10efa7bcaa43c24775f7fb57db40

--- _nuxt/static/1639986076/sr2021/2-bunkers/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639986076",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2021\u002F2-bunkers",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e1bbb598"}}}}(null));
