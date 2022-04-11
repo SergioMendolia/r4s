cf64b7d3171b989c390242d4514a5c89d0b1d49b
SergioMendolia <SergioMendolia@users.noreply.github.com>
2022-04-11 07:18:01 +0000

    deploy: 84d8229c7b7c785757f9da99176ad8c60ae367bf

---
+++ _nuxt/static/1649661479/sr2019/5-anarchy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649661479",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2019\u002F5-anarchy",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9b58e0ad"}}}}(null));
