1bf3ce7625affb2c5afc52a99b991ae9d83e4249
SergioMendolia <SergioMendolia@users.noreply.github.com>
2022-04-27 08:49:01 +0000

    deploy: 856aed655e92d8efdd1f5a65f0e8260699da4151

--- _nuxt/static/1650620720/brawlmachine/1-orbits/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650620720",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbrawlmachine\u002F1-orbits",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ea32e412"}}}}(null));
