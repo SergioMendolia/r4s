21db78dc35ed673c7f48bbb7ac8ab7fbb0caef91
SergioMendolia <SergioMendolia@users.noreply.github.com>
2022-05-11 08:29:25 +0000

    deploy: 713c9150845cb89bc5256a78d4c202467f948809

--- _nuxt/static/1651559586/sr2017/1-pit-ii/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651559586",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2017\u002F1-pit-ii",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"36827b54"}}}}(null));
