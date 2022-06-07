fd6fd6b3fb1dc024a3bdf5211decf32dd635a1ca
SergioMendolia <SergioMendolia@users.noreply.github.com>
2022-06-07 15:34:10 +0000

    deploy: fa9753036c522c960ef9f1d24abc4f530927c480

--- _nuxt/static/1654616039/brawlmachine/6-asteroids/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654616039",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbrawlmachine\u002F6-asteroids",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5472db36"}}}}(null));
