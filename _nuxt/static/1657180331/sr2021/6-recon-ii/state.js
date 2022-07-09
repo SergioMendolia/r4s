ce6654b2eb8ec0609b0cd5f200c9846a7cabd58a
SergioMendolia <SergioMendolia@users.noreply.github.com>
2022-07-09 08:22:21 +0000

    deploy: e641ee611ad97ea73ecd45f54d786db2371af4f0

--- _nuxt/static/1657180331/sr2021/6-recon-ii/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1657180331",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2021\u002F6-recon-ii",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"842c21fd"}}}}(null));
---
+++ _nuxt/static/1657354938/brawlmachine/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1657354938",layout:"default",error:a,serverRendered:false,routePath:"\u002Fbrawlmachine",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9b5df244"}}}}(null));
