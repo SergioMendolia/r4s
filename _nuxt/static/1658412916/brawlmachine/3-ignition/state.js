8562ddf7bd65b0a942da4a29eaa6bf218d57af5e
SergioMendolia <SergioMendolia@users.noreply.github.com>
2022-07-26 08:28:36 +0000

    deploy: 7caeaa3ce968dd6c7c3e2b5c757c8001a657ec00

--- _nuxt/static/1658412916/brawlmachine/3-ignition/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658412916",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbrawlmachine\u002F3-ignition",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e4998ebf"}}}}(null));
