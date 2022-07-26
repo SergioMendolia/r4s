f95dca581c64aea241b7ed1b79ad4681430f4b87
SergioMendolia <SergioMendolia@users.noreply.github.com>
2022-07-26 08:29:25 +0000

    deploy: fe331db03fe1d3aa7b06dc7adeb098c574d7c9b0

--- _nuxt/static/1658824149/sr2019/4-invasion/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658824149",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2019\u002F4-invasion",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d40a3f88"}}}}(null));
