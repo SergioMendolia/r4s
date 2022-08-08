cf81aacdde916d592a73e8de4aaf557f437662a3
SergioMendolia <SergioMendolia@users.noreply.github.com>
2022-08-08 17:39:12 +0000

    deploy: 600ed1b15bec4bed610c19eaf48e9396f2eb4a82

--- _nuxt/static/1658824163/brawlmachine/4-singularity/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658824163",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbrawlmachine\u002F4-singularity",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d6f6989e"}}}}(null));
--- _nuxt/static/1658824163/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658824163",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d6f6989e"}}}}(null,"\u002F"));
