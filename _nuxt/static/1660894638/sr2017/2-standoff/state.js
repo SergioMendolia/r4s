bb4535f25b1109c64564e203ad47be211ad11295
SergioMendolia <SergioMendolia@users.noreply.github.com>
2022-08-19 07:37:20 +0000

    deploy: f0be01c0a9381f8a050e7ab6e63a704dfff19f6a

---
+++ _nuxt/static/1660894638/sr2017/2-standoff/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660894638",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2017\u002F2-standoff",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c13ae558"}}}}(null));
