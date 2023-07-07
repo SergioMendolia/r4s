aa80c57447e169098c9fbd8f9ebdc9b3f860a710
SergioMendolia <SergioMendolia@users.noreply.github.com>
2023-07-07 06:45:23 +0000

    deploy: b4f1cd0bb743f9424410c19abd008e8bbe441e74

--- _nuxt/static/1688712302/sr2017/5-outlast/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1688712302",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2017\u002F5-outlast",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bb65dad3"}}}}(null));
