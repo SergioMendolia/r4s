4e70c9fdb40e8d77778bc1dbd1e056e5a6bd3874
SergioMendolia <SergioMendolia@users.noreply.github.com>
2023-07-07 06:55:37 +0000

    deploy: 9c51d6291e82a68539b65d400d951b6b0b69071c

---
+++ _nuxt/static/1688712935/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1688712935",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"32aef413"}}}}(null,"\u002F"));
