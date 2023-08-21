d8904e6c51c8a7e2b3fd571ac287ea366a8ecb06
SergioMendolia <SergioMendolia@users.noreply.github.com>
2023-08-21 06:05:22 +0000

    deploy: 4554e588b588a791ffd0b5e626350d7510164e6c

--- _nuxt/static/1692597903/sr2019/1-king-of-the-hill/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1692597903",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsr2019\u002F1-king-of-the-hill",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8884be92"}}}}(null));
--- _nuxt/static/1692597903/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1692597903",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8884be92"}}}}(null,"\u002F"));
